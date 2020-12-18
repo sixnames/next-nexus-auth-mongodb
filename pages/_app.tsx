c4dff6525151271a88ffbc04c4f37b61ae68aeaf
sixnames <slava.kirshman@icloud.com>
2020-12-18 18:41:45 +0200

    Initial

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import '../styles/reset.css';
+import type { AppProps } from 'next/app';
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '../apollo/client';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
+
+export default MyApp;
