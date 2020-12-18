5da55d87b638168d6b57146db0b78bf925b75715
sixnames <slava.kirshman@icloud.com>
2020-12-18 18:55:41 +0200

    GraphQL api route added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,9 @@
 import * as React from 'react';
-import '../styles/reset.css';
+import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ApolloProvider } from '@apollo/client';
-import { useApollo } from '../apollo/client';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const apolloClient = useApollo(pageProps.initialApolloState);
-
-  return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
---
+++ pages/api/graphql.ts
@@ -0,0 +1,15 @@
+import { ApolloServer } from 'apollo-server-micro';
+import { schema } from '../../apollo/schema';
+
+const server = new ApolloServer({
+  schema,
+});
+const handler = server.createHandler({ path: '/api/graphql' });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default handler;
