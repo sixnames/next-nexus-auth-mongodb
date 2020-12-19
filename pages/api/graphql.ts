da1c2e3ed66270da279cfd609ef54295280dbdbf
sixnames <slava.kirshman@icloud.com>
2020-12-19 08:59:41 +0200

    cleanup on graphql.ts

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,12 +1,9 @@
 import { ApolloServer } from 'apollo-server-micro';
 import { schema } from '../../schema';
-import { getSession } from 'next-auth/client';
 
 const server = new ApolloServer({
   schema,
   context: (ctx) => {
-    const session = getSession(ctx);
-    console.log(JSON.stringify(session, null, 2));
     return ctx;
   },
 });
